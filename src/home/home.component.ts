adcc470c662c2421ed7b9f1bd97c11f37110ad2c
Wafaa <wafaa.anwar@finway-eg.com>
2025-01-15 13:06:27 +0200

    modify

--- src/home/home.component.ts
+++ src/home/home.component.ts
@@ -9,10 +9,12 @@ import { Router, RouterModule } from '@angular/router';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  test!:string;
 
   constructor(
     private router: Router
   ) {}
+
 
   onStartService() {
     this.router.navigate(['finger-print']);
